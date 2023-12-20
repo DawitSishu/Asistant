ab15a06ac40d644a5a39e5006b2f6e29edcecaca
DawitSishu <dawitalemayhu55@gmail.com>
2023-12-20 12:58:12 -0500

    error handler

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import errorHandler from "./Middlewares/ErrorHandler.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,8 @@ app.use(cors());
 
 app.use(express.json());
 
+
+app.use(errorHandler);
 app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   console.log("running on http://0.0.0.0:5000");
 });
